a77cd5299b2cd4076095147b5ebd38fdbd3286f6
jwagnerhki <jwagnerhki>
2006-10-27 11:01:11 +0000

    show 'address', 4 column

--- util/dump.c
+++ util/dump.c
@@ -8,15 +8,19 @@ int main(void)
 {
   int c;
   int i=0, j=0;
+  int addr = 0;
 
+  printf("%04X: ", addr);
   while ( EOF!=(c=getc(stdin)) ) {
      printf("%02X ", c);
-     i++;
+     i++; addr++;
      if(i>=3) {
         j++;
-        if (j>=3) { j=0; printf("\n"); }
+        if (j>=4) { j=0; printf("\n%04X: ", addr); }
         else { printf("    "); }
         i = 0;
      }
   }
+
 }
+
