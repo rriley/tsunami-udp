d1896a3b33f23da44021ff14b60269fe812f24f9
jwagnerhki <jwagnerhki>
2006-12-02 17:36:31 +0000

    basic units for win32 compatibility layer

---
+++ win32compat/sys/types.h
@@ -0,0 +1,22 @@
+#ifndef __TYPES_H
+#define __TYPES_H
+
+/* 7.18.1.1  Exact-width integer types */
+typedef signed char int8_t;
+typedef unsigned char   u_int8_t;
+typedef unsigned char u_char;
+typedef short  int16_t;
+typedef unsigned short  u_int16_t;
+typedef int  int32_t;
+typedef unsigned   u_int32_t;
+typedef  __int64   int64_t;
+typedef unsigned  __int64   u_int64_t;
+
+
+typedef	long     time_t;
+typedef long     _ssize_t;
+typedef _ssize_t ssize_t;
+typedef  __int64 off64_t;
+typedef  __int64 fpos64_t;
+
+#endif
