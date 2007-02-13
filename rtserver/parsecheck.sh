61d3fa422c4639db8d8cddfd676adfd733e9e7fb
jwagnerhki <jwagnerhki>
2007-02-13 13:47:17 +0000

    quick test case checker

---
+++ rtserver/parsecheck.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+rm parsetest
+gcc parse_evn_filename.c -DUNIT_TEST -o parsetest -lm
+
+./parsetest gre53_Ef_scan035_154d12h43m10s.vsi
+echo "  expect:  1180874590 (in 2007)"
+echo
+./parsetest gre53_Ef_scan035_154124310.vsi
+echo "  expect:  1180874590 (in 2007)"
+echo
+./parsetest R1262_On_037-1240b_2007037124050_flen=5408000000.evn
+echo "  expect:  1170765650"
+echo
+./parsetest R1262_On_037-1240b_2007037_dl=5408000000.vsi           
+echo "  expect:  1170720000"
+echo
+./parsetest R1262_On_037-1240b_2007y037d_dl=5408000000.vsi
+echo "  expect:  1170720000"
+echo
+./parsetest R1262_On_037-1240b_2007y037d12h6m1s_dl=5408000000.vsi  
+echo "  expect:  1170763561"
+echo
+./parsetest gre53_Ef_scan035_2006-11-21T08:45:00_dl=14400000.vsi   
+echo "  expect:  1164098700"
+echo
+./parsetest dummy_Mh_scan01_134330_dl=1500.vsi
+echo "  expect:  1171374210"
+echo
+./parsetest dummy_Mh_scan01_124500.00_dl=1500.vsi
+echo "  expect:  1164098700"
+echo
+
+
